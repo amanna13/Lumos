80b697a5dee1728ec3a41306b02022a864a0cd0a
Ambarish Manna <amanna1041@gmail.com>
2025-04-20 15:08:44 +0530

    Added CORS Policies

---
+++ java-backend/src/main/java/fi/lumos/javabackend/config/GlobalCorsConfig.java
@@ -0,0 +1,19 @@
+package fi.lumos.javabackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+}
